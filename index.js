05ce22e0fdf87d80d03f534d34f1a6a7e007d10a
Leo Liu <aleoliu96@gmail.com>
2023-06-02 23:09:59 -0400

    Added shape logic for svg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer')
 const fs = require("fs")
 const {Circle, Square, Triangle} = require("./lib/shapes.js")
 
-
+// questions for SVG maker
 inquirer.promt([
     {
         type:"maxLength-input",
@@ -17,9 +17,9 @@ inquirer.promt([
     },
     {
         type:"list",
-        name:"shape",
+        name:"shapeEl",
         message:"Please choose from the three shapes",
-        choices:["Circle", "Triangle", "Square"]
+        choices:["Circle", "Square", "Triangle"]
     },
     {
         type:"input",
@@ -27,3 +27,20 @@ inquirer.promt([
         message:"What color would you like for your shape?"
     }
 ])
+//takes responses from above and creates it
+.then(({text, textColor, shapeEl, shapeColor}) => {
+    switch (shape) {
+        case "Circle":
+            shape = new Circle();
+            shape.setColor(shapeColor);
+            break;
+        case "Square":
+            shape = new Square();
+            shape.setColor(shapeColor);
+            break;
+        case "Triangle":
+            shape = new Triangle();
+            shape.setColor(shapeColor);
+            break;
+    }
+})
